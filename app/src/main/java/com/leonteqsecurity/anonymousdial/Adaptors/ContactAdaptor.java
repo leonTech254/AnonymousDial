7a19ab4fbd4aafad0fc2dcabf33d4d05f66b5b3e
leonTech254 <martinleontech23@gmail.com>
2024-02-10 21:08:17 +0300

    --Contact Display UI

--- app/src/main/java/com/leonteqsecurity/anonymousdial/Adaptors/ContactAdaptor.java
+++ app/src/main/java/com/leonteqsecurity/anonymousdial/Adaptors/ContactAdaptor.java
@@ -1,6 +1,35 @@
 package com.leonteqsecurity.anonymousdial.Adaptors;
 
-public class ContactAdaptor {
-    
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.leonteqsecurity.anonymousdial.R;
+
+public class ContactAdaptor extends RecyclerView.Adapter<ContactAdaptor.ContactViewHolder> {
+
+    @NonNull
+    @Override
+    public ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contact_design_list, parent, false);
+        return new ContactViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ContactViewHolder extends RecyclerView.ViewHolder {
+        public ContactViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
